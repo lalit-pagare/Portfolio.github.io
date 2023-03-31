b0e19fd013ab4f6ac9cf75feb97e82ac200378d4
Lalit Pagare <lalitpagarerealme@gmail.com>
2023-03-31 23:58:51 +0530

    Portfolio
    A simple but beautiful Portfolio Webpage with HTML , CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,12 @@
+const project = document.getElementById('pr')
+project.addEventListener('click',function(){
+    window.scrollTo(0,700);
+})
+const resume = document.getElementById('re')
+resume.addEventListener('click',function(){
+    window.scrollTo(0,1350);
+})
+const home = document.getElementById('home')
+home.addEventListener('click',function(){
+    window.scrollTo(0,0)
+})
